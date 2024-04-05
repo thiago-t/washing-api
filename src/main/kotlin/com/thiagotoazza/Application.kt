c4dd09f1a52abb7d93d6c1bb47f0156ecc33a6a8
Thiago Toazza <thiagotoazza@outlook.com>
2024-04-04 21:58:29 -0300

    Initial commit

---
+++ src/main/kotlin/com/thiagotoazza/Application.kt
@@ -0,0 +1,13 @@
+package com.thiagotoazza
+
+import com.thiagotoazza.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
