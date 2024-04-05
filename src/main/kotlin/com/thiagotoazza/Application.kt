ecb89dbc39d86e549f70a8f8ba354bc1002bb114
Thiago Toazza <thiagotoazza@outlook.com>
2024-04-05 01:09:58 -0300

    Setup washing mongo database

--- src/main/kotlin/com/thiagotoazza/Application.kt
+++ src/main/kotlin/com/thiagotoazza/Application.kt
@@ -1,6 +1,7 @@
 package com.thiagotoazza
 
-import com.thiagotoazza.plugins.*
+import com.thiagotoazza.plugins.configureRouting
+import com.thiagotoazza.plugins.configureSerialization
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
